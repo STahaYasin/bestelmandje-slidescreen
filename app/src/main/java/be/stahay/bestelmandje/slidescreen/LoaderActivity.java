07f4995065ee58bac06f730618bd3ca4823fa4ce
TahaYasin <savrantaha@hotmail.com>
2021-02-28 17:06:29 +0100

    2021/02/28 17:06

---
+++ app/src/main/java/be/stahay/bestelmandje/slidescreen/LoaderActivity.java
@@ -0,0 +1,15 @@
+package be.stahay.bestelmandje.slidescreen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LoaderActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loder);
+    }
+}
--- app/src/main/java/be/stahay/bestelmandje/slidescreen/MainActivity.java
+++ app/src/main/java/be/stahay/bestelmandje/slidescreen/MainActivity.java
@@ -1,6 +1,7 @@
 package be.stahay.bestelmandje.slidescreen;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
 /*
@@ -12,5 +13,15 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new Thread(() -> {
+            try {
+                Thread.sleep(2500);
+            } catch ( InterruptedException e ) {
+                e.printStackTrace();
+            } finally {
+                startActivity(new Intent(MainActivity.this, LoaderActivity.class));
+            }
+        }).start();
     }
 }
